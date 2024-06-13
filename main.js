a3cd3048bc86873129d811c0a62fcaa69ba35881
Hashir Akbar <hashirbusinessinfo@gmail.com>
2024-06-13 13:09:13 +0500

    for gh-pages

--- main.js
+++
@@ -1,8 +0,0 @@
-import "./style.css"
-
-const mobileMenu = document.querySelector(".mobile-menu > img")
-const mobileNav = document.querySelector(".mobile-nav")
-
-mobileMenu.addEventListener("click", () => {
-  mobileNav.classList.toggle("hidden")
-})
